a4befc90e9526626b544e8acb6770f72419bc96a
MeepOwned13 <sebokmatyas01@gmail.com>
2024-03-08 17:21:55 +0100

    More harpoon quickbinds

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -134,6 +134,8 @@ vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
