f2a69b1667118f2b0dcf5c5c29690c683dca5114
MeepOwned13 <98529561+MeepOwned13@users.noreply.github.com>
2024-09-24 10:12:27 +0200

    Disable treesitter on large files

--- lua/meepowned/lazy/treesitter.lua
+++ lua/meepowned/lazy/treesitter.lua
@@ -16,6 +16,9 @@ return {
 
                 highlight = {
                     enable = true,
+                    disable = function(lang, bufnr) -- Disable in large buffers
+                        return vim.api.nvim_buf_line_count(bufnr) > 50000
+                    end,
 
                     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
                     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
